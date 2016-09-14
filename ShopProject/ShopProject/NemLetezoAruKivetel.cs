d13ef1759bf395c3a66bd054f3892a87c1d49221
Papuss <mrpapsu@gmail.com>
2016-09-14 09:26:51 +0200

    till Task V

---
+++ ShopProject/ShopProject/NemLetezoAruKivetel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopProject
+{
+    class NemLetezoAruKivetel : BoltKivetel
+    {
+        public NemLetezoAruKivetel(string message)
+        {
+
+        }
+
+    }
+}
---
+++ ShopProject/ShopProject/TulSokLevonasKivetel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopProject
+{
+    class TulSokLevonasKivetel : BoltKivetel
+    {
+        public TulSokLevonasKivetel(string message)
+        {
+
+        }
+
+    }
+}
